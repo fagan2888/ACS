c9ea333b6dab1da6df787a23a18abdc191f849b0
Shruthi Venkatesh <Svenkatesh@uchicago.edu>
2014-01-24 17:14:36 -0500

    Created hh sharing averages

--- hhsharing.py
+++ hhsharing.py
@@ -2,6 +2,7 @@ from __future__ import division
 import csv
 import pandas as pd
 import numpy as np
+import subprocess 
 
 # Will use for filling blank dataframes
 randn = np.random.randn
@@ -25,20 +26,20 @@ def relatives(group):
 def addlAdult(group):
 	(serialid, hhinfo) = group
 	output = pd.DataFrame(index = hhinfo.index.values)
-	age18_criterion = hhinfo['age'].map(lambda x: x >= 18)
-	age24_criterion = hhinfo['age'].map(lambda x: x >= 24)
-	schoolage_criterion = hhinfo['age'].map(lambda x: 18 <= x <= 24)
-	spouse_criterion = hhinfo['relate'].map(lambda x: x not in ['Spouse', 'Head/Householder']) # Figure out this partner criterion 
+	age18_criterion = hhinfo['age'].map(lambda x: x >= 18) # Identify people who are 18 or older
+	age24_criterion = hhinfo['age'].map(lambda x: x > 24) # Identify people who are 24 or older
+	schoolage_criterion = hhinfo['age'].map(lambda x: 18 <= x <= 24) # Identify people who are between 18-24 
+	spouse_criterion = hhinfo['related'].map(lambda x: x not in ['Spouse', 'Head/Householder', 'Unmarried partner']) # Identify people who are not the household head, nor the unmarried partner or spouse of head
 	educ_criterion = hhinfo['schltype'].map(lambda x: x not in ['Public school', 'Private school (1960,1990-2000,ACS,PRCS)'])
-	adults1 = hhinfo[schoolage_criterion & spouse_criterion & educ_criterion] # People aged 18-24 who are not spouses/partners of the head, and are not in school
-	adults2 = hhinfo[age24_criterion & spouse_criterion] 	# People who are older than 24 and not spouses/partners of the head
-	adults_sharing = (adults1.append(adults2)).drop_duplicates(cols = 'pernum') # All adults older than 18 who are not spouses/partners of the head, exluding students aged 18-14
+	adults1 = hhinfo[schoolage_criterion & spouse_criterion & educ_criterion] # People aged 18-24 who are not the head, or the spouses/partners of the head, and are not in school
+	adults2 = hhinfo[age24_criterion & spouse_criterion] 	# People who are older than 24 and not the head, nor the spouses/partners of the head
+	adults_sharing = (adults1.append(adults2)).drop_duplicates(cols = 'pernum') # All adults older than 18 who are not the head, or spouses/partners of the head, exluding students aged 18-14
 	adults_sharing2 = hhinfo[age18_criterion & spouse_criterion] 	# An alternate definition of adults sharing, just to test for fun
 	output['adults_sharing'] = adults_sharing['pernum'].count()
 	output['adults_sharing2'] = adults_sharing2['pernum'].count()
 	return output
 
-# Get the weighted count of a dummy variable in a dataframe
+# Get the weighted count of a variable in a dataframe
 def wcount(df, var, weight): 
 	data = df[var]
 	wt = df[weight]
@@ -48,21 +49,38 @@ def wcount(df, var, weight):
 # Get the weighted average of a variable in a dataframe
 def wavg(df, var, weight): 
 	# Special allowance for rented types: when calculating average rent, ignore "N/A" values
-	# 9999999 is the code for "N/A"
-	data = df[df[var] < 9999999] 
+	data = df[df[var] != "No rent reported"] 
  	wt = df[weight]
 	avg = (data*weight).sum()/ weight.sum()
 	return avg
 
-# Take a dataframe, var, a new var name, and set of labels. Generate new variable. 
+# Run a stata do file with any parameters 
+def dostata(dofile, *params): 
+	cmd = ["C:/Program Files (x86)/Stata12/StataSE-64", "do", dofile]
+	[cmd.append(p) for p in params]
+	subprocess.call(cmd, shell = 'true')
 
+# Compute national averages for varlist
+def computeNatlAvg(df, varlist):
+	weight = df['hhwt']
+	outputkeys = [x for x in varlist if x not in ['hhwt', 'metaread', 'year']]
+	output = {}
+	#print "Computing national average"
+	for v in outputkeys: 
+		avg = (df[v]*weight).sum()/weight.sum()
+		output.update({v: avg})
+	return output
 
-# Recode categorical variables into dummies based on label names
-cat = []
-
-
-
-
+# Compute MSA level avgs for varlist
+def computeMsaAvg(df, varlist): 
+	output = [] 
+	grouped = df.groupby('metaread')
+	for group in grouped: 
+		m, data = group 
+		info = computeNatlAvg(data, varlist)
+		info.update({'msa': m})
+		output.append(info)
+	return output
 
 '''Implement functions below''' 
 ''' 
@@ -100,7 +118,7 @@ varnames =  ['metaread',  # MSA
 #print varnames
 
 # Read in variables of interest from csv to dataframe with correct column labels
-years = [2007, 2008, 2009, 2010, 2011] 
+years = [2007] 
 for y in years: 
 	print str(y)
 	fname = 'M:/IPUMS/hhdata/'+str(y)+'.csv'
@@ -131,9 +149,36 @@ for y in years:
 			# Convert the info from dataframe format to a dict, and write to csv
 			o.writerow({k: (head_dict[k])[x] for k in head_dict for x in head_dict[k]})
 			#collect_dicts.append({k: (head_dict[k])[x] for k in head_dict for x in head_dict[k]})
-''' 	
+	
+# Clean up yearly datasets (recode vars, etc) with stata code 
+dofile = r"M:\IPUMS\hhsharing_panel.do"
+cmd = ["stata", "do", dofile]
+dostata(dofile)
+'''
+#years = [2011]
+years = range(2007,2012)
+for y in years: 
+	# Read each cleaned file into a pandas dataframe 
+	#y = 2007 
+	fname = "M:/IPUMS/hhdata/hhsharing/" + str(y) + "full.csv"
+	f = csv.DictReader(open(fname, 'rb'))
+	header = f.fieldnames 
+	varlist = ['hhwt', 'metaread', 'year', 'sharedhh', 'num_addl']
+	colindices = [header.index(v) for v in varlist]
+	df = pd.read_csv(fname, usecols = colindices)
+	print "Read " + str(y) + " into dataframe"
 
+	# Compute national and msa level averages of variables in varlist 
+	print "Computing national and msa level averages for " + str(y)
+	natlavg = computeNatlAvg(df, varlist)
+	msa_avg = computeMsaAvg(df, varlist)
 
+	# Write the national and msa level averages into a csv file. 
+	print "Writing national and msa level averages for " + str(y)
+	dfnatlavg = pd.DataFrame([natlavg])
+ 	dfnatlavg.to_csv("M:/IPUMS/hhdata/hhsharing_national" + str(y) + ".csv", header = 'true')
 
+ 	df_msa_avg = pd.DataFrame(msa_avg)
+ 	df_msa_avg.to_csv("M:/IPUMS/hhdata/hhsharing/hhsharing_" + str(y) + ".csv", header = 'true') 
 
 
